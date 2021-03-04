63f8948b9827dc1dd7fba5a458657643e1008ea9
evannor <erica.vannorman@gmail.com>
2021-03-04 10:00:42 -0800

    Add two methods, and console log each Course

--- assignment.js
+++ assignment.js
@@ -8,8 +8,19 @@ class Course {
     this.length = length;
     this.price = price;
   }
+
+  pricePerHour() {
+    return this.length / this.price;
+  }
+
+  courseSummary() {
+    return `${this.title} is a great ${this.length} hour course for only \$${this.price}.`;
+  }
 }
 
 const JSCourse = new Course("Javascript Course", 60, 199.99);
 const DrawingCourse = new Course("Drawing Course", 25, 89.99);
-console.log(JSCourse, DrawingCourse);
+console.log(JSCourse.pricePerHour());
+console.log(JSCourse.courseSummary());
+console.log(DrawingCourse.pricePerHour());
+console.log(DrawingCourse.courseSummary());
